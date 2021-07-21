d321a78aa46287358c7b628ebb5e9239c2e9cd6a
JamieDawson <jamierobertdawson@gmail.com>
2021-07-21 10:33:08 -0700

    finished frontend and search bar functionailty

--- crypto-app/src/Coin.js
+++ crypto-app/src/Coin.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import './Coin.css';
 
-const Coin = ({name, image, symbol, price, volume}) => {
-	//console.log(`images` + image);
+const Coin = ({name, price, symbol, marketcap, volume, image, priceChange}) => {
 	return (
 		<div className='coin-container'>
 			<div className='coin-row'>
-				<img src={image} alt='crypto' />
 				<div className='coin'>
+					<img src={image} alt='crypto' />
 					<h1>{name}</h1>
-					<p className='coint-symbol'>{symbol}</p>
+					<p className='coin-symbol'>{symbol}</p>
 				</div>
-				<div className='con-data'>
+				<div className='coin-data'>
 					<p className='coin-price'>${price}</p>
 					<p className='coin-volume'>${volume.toLocaleString()}</p>
+
+					{priceChange < 0 ? (
+						<p className='coin-percent red'>{priceChange.toFixed(2)}%</p>
+					) : (
+						<p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
+					)}
+
+					<p className='coin-marketcap'>
+						Mkt Cap: ${marketcap.toLocaleString()}
+					</p>
 				</div>
 			</div>
 		</div>
